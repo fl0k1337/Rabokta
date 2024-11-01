9a56389e69d64e20901d0069cdbc27a4cb6f08ea
TursunovAA <fdsgfdgfd@mail.ru>
2024-11-01 11:40:16 +0300

    reygoryo

--- src/Zadasha3.java
+++ src/Zadasha3.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Zadasha3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String[] movies = new String[30];
+        int count = 0;
     }
 }
