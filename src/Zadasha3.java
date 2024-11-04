719c17324e7000926a6fbea63aaec3928c490752
fl0k1337 <145494358+fl0k1337@users.noreply.github.com>
2024-11-04 11:23:18 +0300

    Update Zadasha3.java

--- src/Zadasha3.java
+++ src/Zadasha3.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
 
-public class Zadasha3 {
+public class MovieList {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String[] movies = new String[30];
         int count = 0;
+
+        System.out.println("Введите названия фильмов. Для завершения введите 'выход'.");
+        for (int i = 0; i < movies.length; i++) {
+            System.out.print("Введите название фильма: ");
+            String movie = scanner.nextLine();
+
+            if (movie.equals("выход")) {
+                break;
+            }
+            
+            movies[i] = movie;
+            count++;
+        }
+
+        System.out.println("Ваш список фильмов:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(movies[i]);
+        }
     }
 }
