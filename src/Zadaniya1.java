d22e12a0b6057c2c4b67353c8e175a697568bed0
fl0k1337 <145494358+fl0k1337@users.noreply.github.com>
2024-11-04 11:24:41 +0300

    Update Zadaniya1.java

--- src/Zadaniya1.java
+++ src/Zadaniya1.java
@@ -1,5 +1,20 @@
-public class Zadaniya1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class SwedishTaxCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите доход за месяц (SEK): ");
+        int income = scanner.nextInt();
+        int afterTaxIncome;
+        if (income <= 18800) {
+            afterTaxIncome = income - income * 11 / 100;
+        } else if (income <= 468700) {
+            afterTaxIncome = income - income * 20 / 100;
+        } else if (income <= 675700) {
+            afterTaxIncome = income - income * 32 / 100;
+        } else {
+            afterTaxIncome = income - income * 40 / 100;
+        }
+        System.out.println("Доход с учетом налога: " + afterTaxIncome + " SEK");
     }
 }
